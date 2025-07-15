8e306cdf7d7b6101a64db8b24b63be3ba4c7534f
Piyush-Arora03 <piyush.2023ug1113@iiitranchi.ac.in>
2025-07-15 13:34:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { PORT } = require('./config/index');
+const body_parser = require('body-parser');
+
+const app = express();
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const SetUpAndStartServer = async () => {
+    app.use(body_parser.json());
+    app.use(body_parser.urlencoded({ extended: true }));
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+        db.sequelize.sync({ alter: true });
+    });
+
+}
+
+SetUpAndStartServer();
